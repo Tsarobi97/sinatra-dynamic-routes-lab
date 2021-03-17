96adc4d3c0c64fc286d6c92fb691537c7d70445b
RobertObi <robobi97@googlemail.com>
2021-03-16 22:21:53 -0500

    commit tree d44c12e0dd069bd40e0d86a224f20d215b0e5cc8

--- app.rb
+++ app.rb
@@ -1,6 +1,29 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
-  # Write your code here!
+
+get '/reversename/:name' do
+  @x = params[:name]
+  "#{@x.reverse}"
+end
+
+get '/square/:number' do 
+  @poop = params[:number]
+  ok = "#{@poop.to_i * @poop.to_i}"
+  ok
+end
+
+get '/say/:number/:phrase' do
+  @number = params[:number]
+  @phrase = params[:phrase]
+  messi = @number.to_i
+  # binding.pry
+  ronaldo = messi.times do 
+    puts "#{@phrase}"
+  end
+  "#{ronaldo}"
+  end
+
 
 end
